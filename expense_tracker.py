8245e6183c90cd79d1fb830867e7ea5c11ba96c0
Ashmeet <ashmeetchhabra111@gmail.com>
2026-02-15 21:14:01 +0530

    debugging

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,44 +1,25 @@
 import json
 import sys
 import argparse
-from datetime import datetime
+from datetime import datetime, date
 
 FILE_NAME = 'data.json'
 
 # utility
 def load_expenses():
-    with open('r', FILE_NAME) as f:
+    with open(FILE_NAME, 'r') as f:
         expenses = json.load(f)
         return expenses
 
 def save_expenses(expenses):
-    with open('w', FILE_NAME) as f:
+    with open(FILE_NAME, 'w') as f:
         json.dump(expenses, f, indent=4)
 
 def get_next_id(expenses):
+    if not expenses:
+        return 1
     next_id = max(expense['id'] for expense in expenses) + 1
     return next_id
-
-# argparse handlers
-def handle_add(args):
-    expenses = load_expenses(FILE_NAME)
-    add_expense(expenses, args.description, args.amount)
-
-def handle_update(args):
-    expenses = load_expenses(FILE_NAME)
-    update_expense(expenses, args.id, args.description, args.amount)
-
-def handle_delete(args):
-    expenses = load_expenses(FILE_NAME)
-    delete_expense(expenses, args.id)
-    
-def handle_list(args):
-    expenses = load_expenses(FILE_NAME)
-    list_expenses(expenses)
-
-def handle_summary(args):
-    expenses = load_expenses(FILE_NAME)
-    summarize_expenses(expenses, args.month)
 
 # core features
 def add_expense(expenses, description, amount):
@@ -46,9 +27,8 @@ def add_expense(expenses, description, amount):
         'id': get_next_id(expenses),
         'description': description,
         'amount': amount,
-        'date': datetime.date(datetime.now())
+        'date': str(datetime.now().date())
     }
-    
     expenses.append(expense)
     save_expenses(expenses)
 
@@ -63,7 +43,7 @@ def update_expense(expenses, id, description, amount):
 def delete_expense(expenses, id):
     for expense in expenses:
         if expense['id'] == id:
-            expenses = expenses.remove(expense)
+            expenses.remove(expense)
             save_expenses(expenses)
             break
 
@@ -76,12 +56,33 @@ def summarize_expenses(expenses, month=None):
     if month:
         filtered_total = 0
         for expense in expenses:
-            if expense['date'].month == month:
+            if date.fromisoformat(expense['date']).month == month:
                 filtered_total += expense['amount']
         print(f'Total expenses for month {month}: ${filtered_total}')
     else:
         total_expense = sum(expense['amount'] for expense in expenses)
         print(f'Total expenses: ${total_expense}')
+
+# argparse handlers
+def handle_add(args):
+    expenses = load_expenses()
+    add_expense(expenses, args.description, args.amount)
+
+def handle_update(args):
+    expenses = load_expenses()
+    update_expense(expenses, args.id, args.description, args.amount)
+
+def handle_delete(args):
+    expenses = load_expenses()
+    delete_expense(expenses, args.id)
+    
+def handle_list(args):
+    expenses = load_expenses()
+    list_expenses(expenses)
+
+def handle_summary(args):
+    expenses = load_expenses()
+    summarize_expenses(expenses, args.month)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -108,9 +109,7 @@ def main():
     sp_summary.set_defaults(func=handle_summary)
 
     args = parser.parse_args()
-    # print(args.description, args.amount)
-    # print(args.id)
-    # print(args.month)
+    args.func(args)
 
 if __name__ == '__main__':
     main()
